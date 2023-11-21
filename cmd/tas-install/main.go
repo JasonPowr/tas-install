503e7d7f858e95cb18ebba5d67171b16a69967b4
JasonPowr <20076537@mail.wit.ie>
2023-11-21 14:00:23 +0000

    configure certs

--- cmd/tas-install/main.go
+++ cmd/tas-install/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"work/securesign/tas-install/pkg/certs"
 	"work/securesign/tas-install/pkg/keycloak"
 	"work/securesign/tas-install/pkg/kubernetes"
 )
@@ -13,7 +14,13 @@ func main() {
 		log.Fatalf("Failed to initialize Kubernetes client: %v", err)
 	}
 
+	// Install keycloak
 	if err := keycloak.InstallSSOKeycloak(); err != nil {
 		log.Fatalf("Failed to install keycloak: %v", err)
+	}
+
+	// Setup certs
+	if err := certs.SetupCerts(); err != nil {
+		log.Fatalf("Failed to setup certs: %v", err)
 	}
 }
