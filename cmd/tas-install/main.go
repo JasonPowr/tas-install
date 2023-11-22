7fc5c9f17ecfaae1ecfb330187642070a312683c
JasonPowr <20076537@mail.wit.ie>
2023-11-22 09:13:36 +0000

    configure pull secret

--- cmd/tas-install/main.go
+++ cmd/tas-install/main.go
@@ -5,6 +5,7 @@ import (
 	"work/securesign/tas-install/pkg/certs"
 	"work/securesign/tas-install/pkg/keycloak"
 	"work/securesign/tas-install/pkg/kubernetes"
+	"work/securesign/tas-install/pkg/secrets"
 )
 
 func main() {
@@ -22,5 +23,15 @@ func main() {
 	// Setup certs
 	if err := certs.SetupCerts(); err != nil {
 		log.Fatalf("Failed to setup certs: %v", err)
+	}
+
+	// Create sigstore-monitoring namespace
+	if err := kubernetes.CreateNamespace("sigstore-monitoring"); err != nil {
+		log.Fatalf("Failed to create sigstore-monitoring namespace: %v", err)
+	}
+
+	// Configure Pull Secret
+	if err := secrets.ConfigurePullSecret("pull-secret", "sigstore-monitoring"); err != nil {
+		log.Fatalf("Failed to create pull secret: %v", err)
 	}
 }
